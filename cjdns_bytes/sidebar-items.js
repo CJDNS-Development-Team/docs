5e9c722fda2956f31e06548e48e1204ac16585e4
Travis CI User <travis@example.org>
2020-09-28 12:20:30 +0000

    Update documentation

--- cjdns_bytes/sidebar-items.js
+++ cjdns_bytes/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["ParseError",""],["SerializeError",""]],"struct":[["Reader",""],["Writer",""]]});
+initSidebarItems({"enum":[["ExpectedSize","Expected by user size of buffer."],["ParseError","Errors returned when parsing message or its parts (for example, message header) failed"],["SerializeError","Errors returned when serializing message or its parts (for example, message header) failed"]],"struct":[["Reader","Buffer reader"],["Writer","Buffer writer. Wrapper over `Vec<u8`."]]});
