a7bcf8dabafea19a7d3ba02c8a54bb6b576cae30
Travis CI User <travis@example.org>
2021-01-18 08:46:54 +0000

    Update documentation

--- implementors/core/cmp/trait.Ord.js
+++ implementors/core/cmp/trait.Ord.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
 implementors["cjdns_core"] = [{"text":"impl Ord for EncodingSchemeForm","synthetic":false,"types":[]}];
-implementors["cjdns_keys"] = [{"text":"impl Ord for CJDNS_IP6","synthetic":false,"types":[]}];
+implementors["cjdns_keys"] = [{"text":"impl Ord for CJDNS_IP6","synthetic":false,"types":[]},{"text":"impl Ord for CJDNSPrivateKey","synthetic":false,"types":[]},{"text":"impl Ord for CJDNSPublicKey","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/cmp/trait.PartialOrd.js
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
 implementors["cjdns_core"] = [{"text":"impl PartialOrd&lt;EncodingSchemeForm&gt; for EncodingSchemeForm","synthetic":false,"types":[]}];
-implementors["cjdns_keys"] = [{"text":"impl PartialOrd&lt;CJDNS_IP6&gt; for CJDNS_IP6","synthetic":false,"types":[]}];
+implementors["cjdns_keys"] = [{"text":"impl PartialOrd&lt;CJDNS_IP6&gt; for CJDNS_IP6","synthetic":false,"types":[]},{"text":"impl PartialOrd&lt;CJDNSPrivateKey&gt; for CJDNSPrivateKey","synthetic":false,"types":[]},{"text":"impl PartialOrd&lt;CJDNSPublicKey&gt; for CJDNSPublicKey","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
